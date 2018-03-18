bbd5d9f3e4fd1510da910ddd1aefde61b73aeee0
Dmitry <dmitry@wildev.ru>
2018-03-18 16:46:11 +0000

    Исправил шаблон и вывод

--- 1.1/3/main.py
+++ 1.1/3/main.py
@@ -5,10 +5,10 @@ import urllib.request, json, math
 
 data_url = 'https://www.cbr-xml-daily.ru/daily_json.js'
 currency = 'EUR'
-template = 'Курс {}: {}'
+template = 'Курс {0}: {1:.2f}'
 
 with urllib.request.urlopen(data_url) as url:
     data    = json.loads(url.read().decode())
     details = data['Valute'][currency]
     
-    print(template.format(details['Name'], round(details['Value'], 2)))
+    print(template.format(details['Name'], details['Value']))
