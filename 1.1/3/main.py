7e5677f7e0cdc2e877e5acf066c6657f2b5b24ca
Dmitry <dmitry@wildev.ru>
2018-03-18 16:13:27 +0000

    Вывод курса валюты с точностью до двух знаков после запятой

--- 1.1/3/main.py
+++ 1.1/3/main.py
@@ -1,7 +1,7 @@
 # Текущий курс евро
 # https://www.cbr-xml-daily.ru/daily_json.js
 
-import urllib.request, json 
+import urllib.request, json, math
 
 data_url = 'https://www.cbr-xml-daily.ru/daily_json.js'
 currency = 'EUR'
@@ -10,4 +10,4 @@ with urllib.request.urlopen(data_url) as url:
     data    = json.loads(url.read().decode())
     details = data['Valute'][currency]
     
-    print('Курс ', details['Name'], ': ', details['Value'], sep = '')
+    print('Курс ', details['Name'], ': ', round(details['Value'], 2), sep = '', end = '\n\n')
